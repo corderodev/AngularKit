416a25938c85e20b68850bee27c422fe9849001b
Daniel Cordero <dn.lambb@gmail.com>
2024-03-05 01:30:17 -0300

    chore: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
