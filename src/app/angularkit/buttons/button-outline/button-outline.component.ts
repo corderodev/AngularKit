0a5b5b0183a227a9da33a971e17bd1b4e70bb252
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 19:01:31 -0300

    feat: add button outline
    component for the catalog

---
+++ src/app/angularkit/buttons/button-outline/button-outline.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-angularkit-button-outline',
+  standalone: true,
+  imports: [],
+  templateUrl: './button-outline.component.html'
+})
+export class ButtonOutlineComponent {}
