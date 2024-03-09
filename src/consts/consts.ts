29b32d21a8bc7057a2b8adcccfda0181e0d9b5ff
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 18:40:52 -0300

    chore: add consts file

---
+++ src/consts/consts.ts
@@ -0,0 +1,4 @@
+export const urls = Object.freeze({
+  repo: 'https://github.com/corderodev/AngularKit',
+  upcoming_components: 'https://github.com/corderodev/AngularKit/blob/main/README.md/#Upcoming-components',
+})
