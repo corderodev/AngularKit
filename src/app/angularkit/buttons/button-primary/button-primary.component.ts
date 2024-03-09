49cca151421e97a4dad5d69888d3304c5b487bb5
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 19:01:50 -0300

    feat: add button primary
    component for the catalog

---
+++ src/app/angularkit/buttons/button-primary/button-primary.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-angularkit-button-primary',
+  standalone: true,
+  imports: [],
+  templateUrl: './button-primary.component.html'
+})
+export class ButtonPrimaryComponent {}
