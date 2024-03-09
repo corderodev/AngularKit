2ceec37fe9be1c845f8f5fab4838349d7ec60e26
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 18:44:16 -0300

    feat: add hero section

---
+++ src/app/sections/hero/hero.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-section-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero.component.html'
+})
+export class HeroComponent {}
