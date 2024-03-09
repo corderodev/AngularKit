261751a2ad2750efb6f9ffe6d5bacd36e73f6122
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 18:57:38 -0300

    feat: add small alert danger
    component for the catalog

---
+++ src/app/angularkit/alerts/small-alert-danger/small-alert-danger.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-angularkit-small-alert-danger',
+  standalone: true,
+  imports: [],
+  templateUrl: './small-alert-danger.component.html'
+})
+export class SmallAlertDangerComponent {}
