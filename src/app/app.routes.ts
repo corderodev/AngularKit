e2087b39d4edc6f76e7136d2070c7ac6876cf99c
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 19:03:30 -0300

    refactor: add routes for home, alerts and buttons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router'
+import { HomeComponent } from '@pages/home/home.component'
+import { AlertsComponent } from '@pages/alerts/alerts.component'
+import { ButtonsComponent } from '@pages/buttons/buttons.component'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'alerts',
+    component: AlertsComponent
+  },
+  {
+    path: 'buttons',
+    component: ButtonsComponent
+  }
+]
