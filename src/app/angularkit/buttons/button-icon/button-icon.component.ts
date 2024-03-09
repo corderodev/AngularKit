00fa4371dd4fac47ec29fac354cbedb33a03a56c
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 19:00:56 -0300

    feat: add button icon
    component for the catalog

---
+++ src/app/angularkit/buttons/button-icon/button-icon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-angularkit-button-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './button-icon.component.html'
+})
+export class ButtonIconComponent {}
