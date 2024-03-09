8206aad585bf5721da65c1a5318df493641f3bf2
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 19:00:30 -0300

    feat: add button ghost
    component for the catalog

---
+++ src/app/angularkit/buttons/button-ghost/button-ghost.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-angularkit-button-ghost',
+  standalone: true,
+  imports: [],
+  templateUrl: './button-ghost.component.html'
+})
+export class ButtonGhostComponent {}
