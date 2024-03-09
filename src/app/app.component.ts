ab915827e4f6116e4a24d5ecea621e72c0109bfc
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 18:52:40 -0300

    refactor(app): add header, home and footer section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { HomeComponent } from '@pages/home/home.component'
+import { HeaderComponent } from '@sections/header/header.component'
+import { FooterComponent } from '@sections/footer/footer.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'angularkit';
-}
+export class AppComponent {}
