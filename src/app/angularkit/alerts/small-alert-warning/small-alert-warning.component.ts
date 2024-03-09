2a6686aeded25ee58d2686e8034a9f9c3681cf6f
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 18:58:44 -0300

    feat: add small alert warning
    component for the catalog

---
+++ src/app/angularkit/alerts/small-alert-warning/small-alert-warning.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-angularkit-small-alert-warning',
+  standalone: true,
+  imports: [],
+  templateUrl: './small-alert-warning.component.html'
+})
+export class SmallAlertWarningComponent {}
