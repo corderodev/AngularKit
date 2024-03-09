6db959a67ba06584558ab58b1ded74d9324c0af8
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 18:58:14 -0300

    feat: add small alert success
    component for the catalog

---
+++ src/app/angularkit/alerts/small-alert-success/small-alert-success.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-angularkit-small-alert-success',
+  standalone: true,
+  imports: [],
+  templateUrl: './small-alert-success.component.html'
+})
+export class SmallAlertSuccessComponent {}
