79df596b25b05f0c0ac07e628dc9867814fe5c0f
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 19:01:13 -0300

    feat: add button link
    component for the catalog

---
+++ src/app/angularkit/buttons/button-link/button-link.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-angularkit-button-link',
+  standalone: true,
+  imports: [],
+  templateUrl: './button-link.component.html'
+})
+export class ButtonLinkComponent {}
