ef8180e80b6e7d191816c6de8acb259534882674
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 19:02:17 -0300

    feat: add button secondary
    component for the catalog

---
+++ src/app/angularkit/buttons/button-secondary/button-secondary.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-angularkit-button-secondary',
+  standalone: true,
+  imports: [],
+  templateUrl: './button-secondary.component.html'
+})
+export class ButtonSecondaryComponent {}
