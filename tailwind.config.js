8098b16ea2325c06653a8d4635b56a8d9adf6296
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 18:36:09 -0300

    chore: add screens config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
         'sans': ['Inter', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif'],
       },
     },
+    screens: {
+      'xs': '375px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      'wlayout': '1300px',
+      'hero': '644px'
+    }
   },
   plugins: [],
 }
