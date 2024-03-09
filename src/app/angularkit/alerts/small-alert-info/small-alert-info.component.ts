3c86ff6fd93718e7c332c92a5d85c5871b0075c9
Daniel Cordero <dn.lambb@gmail.com>
2024-03-09 18:57:53 -0300

    feat: add small alert info
    component for the catalog

---
+++ src/app/angularkit/alerts/small-alert-info/small-alert-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-angularkit-small-alert-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './small-alert-info.component.html'
+})
+export class SmallAlertInfoComponent {
+
+}
